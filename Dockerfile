68a663314b12e01aeb156be7f7baa65fd67c38f8
Adam S. Leven <25333107+addohm@users.noreply.github.com>
2025-01-30 16:42:29 -0600

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+FROM python:3.14.0a4-alpine3.21
+
+# Prevents django from writing unnecessary files in the container
+ENV PYTHONDONTWRITEBYTECODE=1
+
+# Set an environment variable to unbuffer Python output, aiding in logging and debugging
+ENV PYTHONBUFFERED=1
+
+# Define an environment variable for the web service's port, commonly used in cloud services
+ENV APPPORT 8000
+
+# Set the working directory within the container to /app for any subsequent commands
+WORKDIR /project
+
+# Copy the requirements.txt into the container
+COPY requirements.txt .
+
+# Check for python3 and pip installation and output version
+RUN python3 --version
+RUN pip --version
+
+# Upgrade pip to ensure we have the latest version for installing dependencies
+RUN pip install --upgrade pip
+
+# Update apk and install psycopg2 dependencies
+RUN apk update
+RUN apk add postgresql-dev
+
+# Install dependencies from the requirements.txt file to ensure our Python environment is ready
+RUN pip install -r requirements.txt
+
+# Copy the entire current directory contents into the container at /app
+COPY . .
+
+# # Set the command to run our web service using Gunicorn, binding it to 0.0.0.0 and the PORT environment variable
+CMD gunicorn ${PROJECT_NAME}.wsgi:application --bind 0.0.0.0:"${APPPORT}"
+
+# Inform Docker that the container listens on the specified network port at runtime
+EXPOSE ${APPPORT}
+
+# docker build -t django-docker:latest .
---
+++ cdc_project/settings/development.py
@@ -0,0 +1,130 @@
+from pathlib import Path
+from os import sys
+import mimetypes
+
+from . import get_secret
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-6cgld=9(q4u)r3j7a7mrm*x*#^-%hyn2*6ak$7&h^dgdb-#m)m'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+INTERNAL_IPS = ['10.0.0.250']
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'cdc_project.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'cdc_project.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+# Only enable the toolbar when we're in debug mode and we're
+# not running tests. Django will change DEBUG to be False for
+# tests, so we can't rely on DEBUG alone.
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ENABLE_DEBUG_TOOLBAR = DEBUG and "test" not in sys.argv
+if ENABLE_DEBUG_TOOLBAR:
+    SECURE_CROSS_ORIGIN_OPENER_POLICY = None
+    mimetypes.add_type("application/javascript", ".js", True)
+    INSTALLED_APPS += [
+        "debug_toolbar",
+    ]
+    MIDDLEWARE += [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    ]
+    # Customize the config to support turbo and htmx boosting.
+    DEBUG_TOOLBAR_CONFIG = {"ROOT_TAG_EXTRA_ATTRS": "data-turbo-permanent hx-preserve"}
